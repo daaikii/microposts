75fdb9581d9aaa82ecfd1074be7a777e30701b2f
ikeda <ikedaaa0210@icloud.com>
2020-11-21 06:10:39 +0000

    repair

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  befour_action:require_user_logged_in
+  before_action:require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
   def create
